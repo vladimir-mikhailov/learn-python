4c962c21ae92b912961d6fc2dcf7ba7c9b8882f0
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-10-13 01:02:55 +0300

    refactor: s2 hw2 task5

--- geekbrains/Seminar_2/HomeWork/home_work_5.py
+++ geekbrains/Seminar_2/HomeWork/home_work_5.py
@@ -5,21 +5,20 @@ from random import randint
 while True:
     try:
         length = int(
-            input('Введите длину списка случайных чисел (или 0, чтобы выйти): '))
+            input('Введите длину списка случайных чисел (или любой символ, чтобы выйти): '))
     except ValueError:
-        print('Введите целое число или 0, чтобы завершить.')
+        print('Good Bye!')
+        break
 
     if length == 0:
         print('Good Bye!')
         break
 
     lst = [randint(0, 101) for i in range(length)]
-
-    random_indices = set([])
-    while len(random_indices) < length:
-        random_indices.add(str(randint(0, length - 1)))
-
-    sorted_list = [lst[int(i)] for i in random_indices]
-
     print(f'Исходный список:     {lst}')
-    print(f'Перемешанный список: {sorted_list}')
+
+    for i in range(length):
+        rnd = randint(0, length - 1)
+        lst[i], lst[rnd] = lst[rnd], lst[i]
+
+    print(f'Перемешанный список: {lst}')
