9e41540c2cbb485e3e6169406efa49b8b0003f55
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-10-13 21:20:32 +0300

    s3 hw3

--- geekbrains/Seminar_3/HomeWork/3.py
+++ geekbrains/Seminar_3/HomeWork/3.py
@@ -4,3 +4,13 @@
 # Пример:
 
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+from random import randint
+
+n = int(input('Длина списка: '))
+
+lst = [str(randint(0, 1000) / 100) for i in range(n)]
+
+afterpoints = [round(float(el) - float(el.split('.')[0]), 2) for el in lst]
+
+print(f'{lst} -> {max(afterpoints) - min(afterpoints)}')
