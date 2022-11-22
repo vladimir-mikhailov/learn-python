5d15b2012368caf2acff757356d6648e9e2aec06
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-11-22 23:26:44 +0300

    fix: hide TOKEN to env variable

---
+++ geekbrains/Seminar9/Classwork/README.md
@@ -0,0 +1,17 @@
+# Telegram Bot
+
+## Requirements
+
+This installs the pre-release of v20 python-telegram-bot
+
+`pip install python-telegram-bot --pre`
+
+or
+
+`pip install -r requirements.txt`
+
+## Run & Token
+
+Run like this with your own token:
+
+`TOKEN=your_token_here python bot.py`
--- geekbrains/Seminar9/Classwork/bot.py
+++ geekbrains/Seminar9/Classwork/bot.py
@@ -1,25 +1,16 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from change import change
+
+TOKEN = os.environ["TOKEN"]
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(change(update.message.text))
 
-app = ApplicationBuilder().token(
-    "Your_Token here").build()
+app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("hello", hello))
 
 app.run_polling()
-
-# /hello абв фывафыва фыва
-
-# https://t.me/vladimir_superbot
-
-# This installs the pre-release of v20
-
-# pip install python-telegram-bot --pre
-# python bot.py
-
-# https://t.me/BotFather
