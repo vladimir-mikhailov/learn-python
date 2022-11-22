24ee1ef3d643a93b94c51da9d4c6700fe2181305
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-11-22 22:12:54 +0300

    upd classwork 9

--- geekbrains/Seminar9/Classwork/bot.py
+++ geekbrains/Seminar9/Classwork/bot.py
@@ -1,31 +1,25 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from task1 import change
+from change import change
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    txt = update.message.text
-
-    letters = 'абв'
-
-    words_only = [x for x in txt.translate(
-        str.maketrans('.,!-_–:;()', '          ')).split()]
-
-    for word in words_only:
-        if set(letters.casefold()).issubset(set(word.casefold())):
-            txt = txt.replace(word, '').replace('  ', ' ').strip()
-
-    if txt.startswith((',', '.', '–')):
-        txt = txt[1:].lstrip()
-
-    await update.message.reply_text(txt.capitalize())
-
+    await update.message.reply_text(change(update.message.text))
 
 app = ApplicationBuilder().token(
-    "5841890239:AAH2k52U3kWDYA6a51kdorZ1F1gNr9lz7Ks").build()
+    "Your_Token here").build()
 
 app.add_handler(CommandHandler("hello", hello))
 
 app.run_polling()
 
 # /hello абв фывафыва фыва
+
+# https://t.me/vladimir_superbot
+
+# This installs the pre-release of v20
+
+# pip install python-telegram-bot --pre
+# python bot.py
+
+# https://t.me/BotFather
